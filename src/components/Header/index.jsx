0c5ac2fc721d84f9dc40dc1791fbfa6677ecc5ca
Alex D. <t.docheff@gmail.com>
2023-12-07 22:13:11 -0300

    header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,10 @@ import './styles.css'
 export default function Header() {
     return (
         <header>
-            <h1>Pokedex</h1>
+            <img src="../public/images/pokeball.png" alt="pokeball logo" />
+            <h1>Pokedex</h1> 
+            <input className="searchBar" type="text" />
+            <button><img src="../public/images/Arrow.svg" alt="" /></button>
         </header>
     )
 }
